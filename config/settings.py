4b5b69f0a27c5b446781b657df19c69aa43430cd
Allan Rafael <allanrafaelfo@gmail.com>
2021-01-25 19:03:07 -0300

    Adiciona bibliotecas para o projeto

--- config/settings.py
+++ config/settings.py
@@ -31,13 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django admin
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Project apps
     'catalog.apps.CatalogConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
